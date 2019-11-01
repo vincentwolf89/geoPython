ff3ed38e7ee4548e6c08746def998dc67eff5508
vincentwolf89 <Ravestein1!>
2019-11-01 12:45:11 +0100

    Update

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 import arcpy
 
+arcpy.env.workspace = 'C:/Users/vince/Desktop/GIS/temp.gdb'
+
+mxd = r'C:\Users\vince\Desktop\test.mxd'
+out_gdb = r'C:\Users\vince\Desktop\GIS\temp.gdb'
+
+mxd = arcpy.mapping.MapDocument(mxd)
+# df = arcpy.mapping.ListDataFrames(mxd)
+for lyr in arcpy.mapping.ListLayers(mxd):
+    if "tussen" in lyr.name:
+        print lyr.name
+        arcpy.FeatureClassToFeatureClass_conversion(lyr, out_gdb, str(lyr)+"_kopie")
+
+arcpy.SelectLayerByLocation_management('nwo_temp', 'intersect', zone)
+
