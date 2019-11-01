98d777d9f01d3ac25419e87604db4d0f2f22cc9e
vincentwolf89 <Ravestein1!>
2019-11-01 21:30:58 +0100

    Update

---
+++ runner.py
@@ -0,0 +1,34 @@
+import gc
+gc.collect()
+from basisfuncties import *
+
+arcpy.env.overwriteOutput = True
+
+
+arcpy.env.workspace = r'C:\Users\vince\Desktop\GIS\test.gdb'
+
+profielen = 'profielen_reg'
+invoerpunten = 'punten_profielen'
+uitvoerpunten = 'punten_profielen_z'
+stapgrootte_punten = 1
+raster = r'C:\Users\vince\Desktop\GIS\losse rasters\ahn3clip\ahn3clip_safe'
+trajectlijn = 'lijn_regionaal'
+code = 'dv_nummer'
+resultfile = "C:/Users/vince/Desktop/testprofielen.xls"
+
+
+
+# copy_trajectory_lr(trajectlijn,code)
+# set_measurements_trajectory(profielen,trajectlijn,code,stapgrootte_punten)
+# extract_z_arcpy(invoerpunten,uitvoerpunten,raster)
+# add_xy(uitvoerpunten,code)
+# to_excel(uitvoerpunten,resultfile)
+
+
+puntenset = 'punten_binnenkruin'
+# create_contour_lines(trajectlijn,raster,'contour_safe',0.5)
+# snap_to_contour(puntenset,'contour_safe','near')
+
+kruinhoogte_groepen(uitvoerpunten,stapgrootte_punten)
+max_kruinhoogte(uitvoerpunten,profielen)
+
