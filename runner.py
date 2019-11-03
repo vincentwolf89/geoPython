2e5ae75a593b3c077a9f862f36ef0b2c4cf37f88
Vincent <vincent.vanderwolf@gmail.com>
2019-11-03 23:01:28 +0100

    Aanpassing

--- runner.py
+++ runner.py
@@ -5,16 +5,17 @@ from basisfuncties import *
 arcpy.env.overwriteOutput = True
 
 
-arcpy.env.workspace = r'D:\GIS\test.gdb'
+arcpy.env.workspace = r'D:\Projecten\HDSR\data\werk.gdb'
 
-profielen = 'profielen_reg'
+profielen = 'testprofielen_hdsr'
 invoerpunten = 'punten_profielen'
 uitvoerpunten = 'punten_profielen_z'
-stapgrootte_punten = 1
-raster = r'D:\GIS\losse rasters\ahn3clip\ahn3clip_safe'
-trajectlijn = 'lijn_regionaal'
+stapgrootte_punten = 0.5
+afronding = 1
+raster = r'D:\Projecten\HDSR\data\ahn_hdsr.gdb\AHN3grondfilter'
+trajectlijn = 'test_traject_hdsr'
 code = 'dv_nummer'
-resultfile = "C:/Users/Vincent/Desktop/testprofielen.xls"
+resultfile = r"C:\Users\Vincent\Desktop\testprofielen.xls"
 
 
 
@@ -24,11 +25,13 @@ resultfile = "C:/Users/Vincent/Desktop/testprofielen.xls"
 # add_xy(uitvoerpunten,code)
 # to_excel(uitvoerpunten,resultfile)
 
+# kruinhoogte_groepen(uitvoerpunten,stapgrootte_punten,afronding)
+# max_kruinhoogte(uitvoerpunten,profielen)
+
+
+
+
 
 puntenset = 'punten_binnenkruin'
 # create_contour_lines(trajectlijn,raster,'contour_safe',0.5)
 # snap_to_contour(puntenset,'contour_safe','near')
-
-kruinhoogte_groepen(uitvoerpunten,stapgrootte_punten)
-max_kruinhoogte(uitvoerpunten,profielen)
-
