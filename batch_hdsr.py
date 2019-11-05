b8343936d3d3e8d29c5de18a16abbefe2ee89fc9
Vincent <vincent.vanderwolf@gmail.com>
2019-11-05 21:32:05 +0100

    Aanpassing

--- batch_hdsr.py
+++ batch_hdsr.py
@@ -14,43 +14,34 @@ arcpy.env.overwriteOutput = True
 
 profiel_interval = 15
 profiel_lengte = 35
-
 invoerpunten = 'punten_profielen'
-
 stapgrootte_punten = 0.5
 afronding = 1
 raster = r'D:\Projecten\HDSR\data\ahn_hdsr.gdb\AHN3grondfilter'
 code = 'SUBSECT_ID'
-
-
-
-
-
-
-
-
 trajecten = 'trajecten_test'
+
+
 with arcpy.da.SearchCursor(trajecten,['SHAPE@','SUBSECT_ID']) as cursor:
     for row in cursor:
+        # lokale variabelen per dijktraject
         code = 'SUBSECT_ID'
         id = row[1]
         trajectlijn = 'deeltraject_'+str(row[1])
         profielen = 'profielen_'+str(row[1])
         uitvoerpunten = 'punten_profielen_z_'+str(row[1])
         resultfile = 'C:/Users/Vincent/Desktop/xls_uitvoer/'+str(row[1])+'.xls'
-
         where = '"' + code + '" = ' + "'" + str(id) + "'"
-        # arcpy.CopyFeatures_management(row[0], trajectlijn)
+
+        # selecteer betreffend traject
         arcpy.Select_analysis(trajecten, trajectlijn, where)
 
-
+        # doorlopen stappen
         generate_profiles(profiel_interval, profiel_lengte, trajectlijn, code, profielen)
         copy_trajectory_lr(trajectlijn, code)
         set_measurements_trajectory(profielen, trajectlijn, code, stapgrootte_punten)
         extract_z_arcpy(invoerpunten, uitvoerpunten, raster)
         add_xy(uitvoerpunten, code)
         to_excel(uitvoerpunten, resultfile)
-
         kruinhoogte_groepen(uitvoerpunten, stapgrootte_punten, afronding, code)
         max_kruinhoogte(uitvoerpunten, profielen, code)
-        # break
