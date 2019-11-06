a567808e5f6ac4236331d4a3cf436affa5238988
Vincent <vincent.vanderwolf@gmail.com>
2019-11-06 23:45:14 +0100

    Aanpassing

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import arcpy
 import math
+import numpy as np
 from arcpy.sa import *
 import xlwt
 import pandas as pd
@@ -7,50 +8,66 @@ from itertools import groupby
 # uitzetten melding pandas
 pd.set_option('mode.chained_assignment', None)
 from basisfuncties import*
+import matplotlib.pyplot as plt
 
-arcpy.env.workspace = r'D:\Projecten\HDSR\data\test_batch.gdb'
+arcpy.env.workspace = r'D:\Projecten\HDSR\data\werk.gdb'
 arcpy.env.overwriteOutput = True
 
-
-profiel_interval = 15
-profiel_lengte = 35
-
-invoerpunten = 'punten_profielen'
-
-stapgrootte_punten = 0.5
-afronding = 1
-raster = r'D:\Projecten\HDSR\data\ahn_hdsr.gdb\AHN3grondfilter'
+invoer = 'punten_profielen_z'
 code = 'SUBSECT_ID'
+afstanden_punten = 0.5
+
+array = arcpy.da.FeatureClassToNumPyArray(invoer, ('OBJECTID', 'profielnummer',code, 'afstand', 'z_ahn'))
+df = pd.DataFrame(array)
+df2 = df.dropna()
+sorted = df2.sort_values(['profielnummer', 'afstand'], ascending=[True, True])
+grouped = sorted.groupby('profielnummer')
+
+for name, group in grouped:
+    # print group
+
+    group['verschil'] = abs(group['z_ahn'].diff())
+    group['gem_verschil'] = group.iloc[:, 5].rolling(window=3).mean()
+
+    dx_dt = np.gradient(group['afstand'])
+    dy_dt = np.gradient(group['z_ahn'])
+    velocity = np.array([[dx_dt[i], dy_dt[i]] for i in range(dx_dt.size)])
+
+    ds_dt = np.sqrt(dx_dt * dx_dt + dy_dt * dy_dt)
+    tangent = np.array([1 / ds_dt] * 2).transpose() * velocity
+
+    tangent_x = tangent[:, 0]
+    tangent_y = tangent[:, 1]
+
+    deriv_tangent_x = np.gradient(tangent_x)
+    deriv_tangent_y = np.gradient(tangent_y)
+
+    dT_dt = np.array([[deriv_tangent_x[i], deriv_tangent_y[i]] for i in range(deriv_tangent_x.size)])
+
+    length_dT_dt = np.sqrt(deriv_tangent_x * deriv_tangent_x + deriv_tangent_y * deriv_tangent_y)
+
+    normal = np.array([1 / length_dT_dt] * 2).transpose() * dT_dt
+
+    d2s_dt2 = np.gradient(ds_dt)
+    d2x_dt2 = np.gradient(dx_dt)
+    d2y_dt2 = np.gradient(dy_dt)
+
+    curvature = np.abs(d2x_dt2 * dy_dt - dx_dt * d2y_dt2) / (dx_dt * dx_dt + dy_dt * dy_dt) ** 1.5
+    t_component = np.array([d2s_dt2] * 2).transpose()
+    n_component = np.array([curvature * ds_dt * ds_dt] * 2).transpose()
+
+    acceleration = t_component * tangent + n_component * normal
+
+    print acceleration
 
 
 
 
 
 
+    plt.plot(group['afstand'], group['z_ahn'])
 
+    print group
+    # plt.show()
+    break
 
-trajecten = 'trajecten_test'
-with arcpy.da.SearchCursor(trajecten,['SHAPE@','SUBSECT_ID']) as cursor:
-    for row in cursor:
-        code = 'SUBSECT_ID'
-        id = row[1]
-        trajectlijn = 'deeltraject_'+str(row[1])
-        profielen = 'profielen_'+str(row[1])
-        uitvoerpunten = 'punten_profielen_z_'+str(row[1])
-        resultfile = 'C:/Users/Vincent/Desktop/xls_uitvoer/'+str(row[1])+'.xls'
-
-        where = '"' + code + '" = ' + "'" + str(id) + "'"
-        # arcpy.CopyFeatures_management(row[0], trajectlijn)
-        arcpy.Select_analysis(trajecten, trajectlijn, where)
-
-
-        generate_profiles(profiel_interval, profiel_lengte, trajectlijn, code, profielen)
-        copy_trajectory_lr(trajectlijn, code)
-        set_measurements_trajectory(profielen, trajectlijn, code, stapgrootte_punten)
-        extract_z_arcpy(invoerpunten, uitvoerpunten, raster)
-        add_xy(uitvoerpunten, code)
-        to_excel(uitvoerpunten, resultfile)
-
-        kruinhoogte_groepen(uitvoerpunten, stapgrootte_punten, afronding, code)
-        max_kruinhoogte(uitvoerpunten, profielen, code)
-        # break
