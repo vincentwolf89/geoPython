efb96c3c90fb95fdd9bb97559b19ba7657ff3ef1
Vincent <vincent.vanderwolf@gmail.com>
2019-11-06 22:00:19 +0100

    Aanpassing

--- runner.py
+++ runner.py
@@ -20,15 +20,18 @@ code = 'SUBSECT_ID'
 resultfile = r"C:\Users\Vincent\Desktop\testprofielen.xls"
 uitvoer_maxpunten = 'max_kruinhoogte'
 
-generate_profiles(profiel_interval, profiel_lengte, trajectlijn,code,profielen)
-copy_trajectory_lr(trajectlijn,code)
-set_measurements_trajectory(profielen,trajectlijn,code,stapgrootte_punten)
-extract_z_arcpy(invoerpunten,uitvoerpunten,raster)
-add_xy(uitvoerpunten,code)
-to_excel(uitvoerpunten,resultfile)
+profiel_lengte_land = 20
+profiel_lengte_rivier = 10
 
-kruinhoogte_groepen(uitvoerpunten,stapgrootte_punten,afronding,code)
-max_kruinhoogte(uitvoerpunten,profielen,code,uitvoer_maxpunten)
+generate_profiles(profiel_interval, profiel_lengte_land, profiel_lengte_rivier, trajectlijn,code,profielen)
+# copy_trajectory_lr(trajectlijn,code)
+# set_measurements_trajectory(profielen,trajectlijn,code,stapgrootte_punten)
+# extract_z_arcpy(invoerpunten,uitvoerpunten,raster)
+# add_xy(uitvoerpunten,code)
+# to_excel(uitvoerpunten,resultfile)
+#
+# kruinhoogte_groepen(uitvoerpunten,stapgrootte_punten,afronding,code)
+# max_kruinhoogte(uitvoerpunten,profielen,code,uitvoer_maxpunten)
 
 
 
