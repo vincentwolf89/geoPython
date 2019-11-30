a7bdb26f4e1d85daf9ecbf43c864863d72eb4800
Vincent <vincent.vanderwolf@gmail.com>
2019-11-30 14:53:20 +0100

    Aanpassing

--- HDSR/batch_hdsr.py
+++ HDSR/batch_hdsr.py
@@ -8,7 +8,7 @@ from itertools import groupby
 pd.set_option('mode.chained_assignment', None)
 from basisfuncties import*
 
-arcpy.env.workspace = r'D:\Projecten\HDSR\data\oplevering_v1.gdb'
+arcpy.env.workspace = r'D:\Projecten\HDSR\data\oplevering_hdsr.gdb'
 arcpy.env.overwriteOutput = True
 
 
@@ -24,13 +24,14 @@ max_afstand = 5
 min_achterland = 5
 max_achterland = 20
 raster = r'D:\Projecten\HDSR\data\ahn_hdsr.gdb\AHN3grondfilter'
+bodemdalingskaart = r'D:\GIS\losse rasters\bodemdalingskaart_app_data_geotiff_Bodemdalingskaart_10de_percentiel_mm_per_jaar_verticale_richting_v2018002.tif'
 code_hdsr = 'Naam'
 toetspeil = 'TP2024'
-trajecten = 'test'
+trajecten = 'test_140'
 # specifieke invoer bepaling bit/but
 verschil_maxkruin = 0.2
-excelmap = 'C:/Users/Vincent/Desktop/xlsx_uitvoer/'
-
+excelmap = 'D:/Projecten/HDSR/oplevering/xlsx_uitvoer/'
+totaal_profielen = []
 with arcpy.da.SearchCursor(trajecten,['SHAPE@',code_hdsr]) as cursor:
     for row in cursor:
         # lokale variabelen per dijktraject
@@ -51,15 +52,22 @@ with arcpy.da.SearchCursor(trajecten,['SHAPE@',code_hdsr]) as cursor:
         arcpy.Select_analysis(trajecten, trajectlijn, where)
 
         # doorlopen stappen
-        generate_profiles(profiel_interval, profiel_lengte_land, profiel_lengte_rivier, trajectlijn, code_hdsr,toetspeil, profielen)
-        copy_trajectory_lr(trajectlijn, code_hdsr)
-        set_measurements_trajectory(profielen, trajectlijn, code_hdsr, stapgrootte_punten,toetspeil)
-        extract_z_arcpy(invoerpunten, uitvoerpunten, raster)
-        add_xy(uitvoerpunten, code_hdsr)
-        # to_excel(uitvoerpunten, resultfile,sorteervelden='profielnummer A; afstand A')
-        excel_writer(uitvoerpunten, code, excel, id)
-        kruinhoogte_groepen(uitvoerpunten, stapgrootte_punten, afronding, code_hdsr)
-        max_kruinhoogte_test(uitvoerpunten, profielen, code_hdsr,uitvoer_maxpunten,min_afstand,max_afstand,toetspeil)
-        kruinbepalen(uitvoerpunten,code_hdsr,uitvoer_binnenkruin,uitvoer_buitenkruin,verschil_maxkruin,min_afstand,max_afstand)
-        binnenteenbepalen(uitvoerpunten, code_hdsr, min_achterland, max_achterland, uitvoer_binnenteen, min_afstand,max_afstand,uitvoer_binnenkruin)
-        koppeling_hbn_hdsr(profielen,toetspeil)
+        # generate_profiles(profiel_interval, profiel_lengte_land, profiel_lengte_rivier, trajectlijn, code_hdsr,toetspeil, profielen)
+        # copy_trajectory_lr(trajectlijn, code_hdsr)
+        # set_measurements_trajectory(profielen, trajectlijn, code_hdsr, stapgrootte_punten,toetspeil)
+        # extract_z_arcpy(invoerpunten, uitvoerpunten, raster)
+        # add_xy(uitvoerpunten, code_hdsr)
+        # excel_writer(uitvoerpunten, code, excel, id)
+        # kruinhoogte_groepen(uitvoerpunten, stapgrootte_punten, afronding, code_hdsr)
+        # max_kruinhoogte_test(uitvoerpunten, profielen, code_hdsr,uitvoer_maxpunten,min_afstand,max_afstand,toetspeil)
+        # kruinbepalen(uitvoerpunten,code_hdsr,uitvoer_binnenkruin,uitvoer_buitenkruin,verschil_maxkruin,min_afstand,max_afstand)
+        # binnenteenbepalen(uitvoerpunten, code_hdsr, min_achterland, max_achterland, uitvoer_binnenteen, min_afstand,max_afstand,uitvoer_binnenkruin)
+        # koppeling_hbn_hdsr(profielen,toetspeil)
+        bereken_restlevensduur(profielen,bodemdalingskaart)
+        print profielen
+
+        # totaal_profielen.append(profielen)
+        # print profielen
+
+
+# arcpy.Merge_management(totaal_profielen, 'profielen_RWK_areaal_2024')
