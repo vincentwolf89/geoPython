f755e76d3a95cac3721aa5e4675ff5cb87fb5e6c
Vincent <vincent.vanderwolf@gmail.com>
2020-03-24 22:52:22 +0100

    Aanpassing

--- WSRL/sprokSplitOordeel.py
+++ WSRL/sprokSplitOordeel.py
@@ -11,15 +11,19 @@ arcpy.env.overwriteOutput = True
 
 puntenOordeel ="OutputAggregatiePerUittredepunt"
 profielen = "profielen_ss"
+trajectLijn = "trajectlijn"
 veldenOordeelPunten = ['Eindoordeel','profielnummer']
 veldenProfielen = ['profielnummer','eindoordeelPiping']
 lijstOnvoldoende = ["IIIv","Ivv","Vv","VIv","Voldoet niet vanwege dijkbasisregel"]
 
 outputPunten = "OordeelProfielNummer"
+outputOordeelLijn = "oordeel1"
 
 def koppelingPunten(puntenOordeel,profielen,outputPunten):
     # koppel profielnummer aan punten, dichtstbijzijnde
     arcpy.SpatialJoin_analysis(puntenOordeel, profielen, outputPunten, "JOIN_ONE_TO_ONE", "KEEP_ALL","",match_option="CLOSEST")
+
+    print "Oordeel van punten gekoppeld aan profielnummers"
 
 def oordeelProfiel(outputPunten, veldenOordeelPunten, lijstOnvoldoende,profielen, veldenProfielen):
     df = pd.DataFrame(columns=['profielnummer','eindoordeel'])
@@ -52,8 +56,29 @@ def oordeelProfiel(outputPunten, veldenOordeelPunten, lijstOnvoldoende,profielen
                 row[1] = "voldoende"
             cursor.updateRow(row)
 
-# isects trajectlijn profielen
-# split trajectlijn at isects
-# midpoints splits
-# koppel oordeel splits, merge splits met zelfde oordeel!
+    print "Oordeel van punten per profiel vastgesteld"
+
+def eindOordeel(trajectLijn, profielen, outputOordeelLijn):
+    # isects trajectlijn profielen
+    arcpy.Intersect_analysis([trajectLijn,profielen], "tempIsect", "ALL", "", "POINT")
+
+
+    # split trajectlijn at isects
+    arcpy.SplitLineAtPoint_management(trajectLijn, "tempIsect", 'splitProfielen', 1)
+
+    # midpoints splits
+    arcpy.FeatureVerticesToPoints_management("splitProfielen", "splitProfielenPunten", "MID")
+
+    # split trajectlijn tussen profielen
+    arcpy.SplitLineAtPoint_management(trajectLijn, "splitProfielenPunten", 'splitTussen', 1)
+    # koppel oordeel splits
+    arcpy.SpatialJoin_analysis('splitTussen', profielen, "splitTussenOordeel", "JOIN_ONE_TO_ONE", "KEEP_ALL","",match_option="CLOSEST")
+    # dissolve op eindoordeel
+    arcpy.Dissolve_management("splitTussenOordeel", outputOordeelLijn, "eindoordeelPiping", "", "MULTI_PART", "DISSOLVE_LINES")
+
+    print "Eindoordeel voor trajectlijn bepaald"
+
+koppelingPunten(puntenOordeel,profielen,outputPunten)
+oordeelProfiel(outputPunten, veldenOordeelPunten, lijstOnvoldoende,profielen, veldenProfielen)
+eindOordeel(trajectLijn,profielen,outputOordeelLijn)
 
