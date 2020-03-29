dc62593e2956a1db8bcc9a2c0429a422234f8fb1
Vincent <vincent.vanderwolf@gmail.com>
2020-03-29 14:47:58 +0200

    Aanpassing

--- GIS/waterlopen_opbouw.py
+++ GIS/waterlopen_opbouw.py
@@ -1,4 +1,5 @@
 import arcpy
+from basisfuncties import *
 from arcpy.sa import *
 
 arcpy.CheckOutExtension("Spatial")
@@ -6,50 +7,69 @@ arcpy.CheckOutExtension("Spatial")
 arcpy.env.workspace = r'D:\GoogleDrive\WSRL\test_vergridden.gdb'
 arcpy.env.overwriteOutput = True
 
-lijn = "waterloop_lijn_13"
-vlak = "waterloop_13"
-smooth = "waterloop_lijn_13_smooth"
+lijn = "waterloop_lijn_21"
+vlak = "waterloop_21"
+smooth = "waterloop_lijn_21_smooth"
+tolerance = 0.5
+dist_mini_buffer = -0.2
 
 waterlopen = "waterlopen_safe"
 
 ## middenvlak creeren ##
-# waterloop afronden (lijn)
-arcpy.SmoothLine_cartography(lijn, smooth, "PAEK", "10 Meters", "FIXED_CLOSED_ENDPOINT", "NO_CHECK")
+
+# waterloop buffer afronden (lijn en polygoon)
+arcpy.SmoothLine_cartography(lijn, "line_smooth", "PAEK", "10 Meters", "FIXED_CLOSED_ENDPOINT", "NO_CHECK")
+arcpy.SmoothPolygon_cartography(vlak, "poly_smooth", "PAEK", "10 Meters", "FIXED_ENDPOINT", "NO_CHECK")
 # euclidean raster
-arcpy.gp.EucDistance_sa(smooth, "temp_euclidean", "", "0,25","")
+arcpy.gp.EucDistance_sa("line_smooth", "temp_euclidean", "", "0,1","")
 # slope euclidean raster
 arcpy.gp.Slope_sa("temp_euclidean", "temp_slope", "DEGREE", "1")
 # rastercalc
 raster = arcpy.Raster("temp_slope")
-outraster = raster <= 38
+outraster = raster <= 42
 outraster.save("temp_rastercalc")
 # clip raster with polygon
-arcpy.Clip_management("temp_rastercalc", "", "temp_clip", vlak, "127", "ClippingGeometry", "MAINTAIN_EXTENT")
+# eerst minibuffer op polygoon
+arcpy.Buffer_analysis("poly_smooth", "temp_buffer_smnooth", dist_mini_buffer, "OUTSIDE_ONLY", "FLAT", "NONE", "", "PLANAR")
+# minibuffer binnekant bewaren
+list_oid = []
+arcpy.FeatureToLine_management("temp_buffer_smnooth", "temp_buffer_smnooth_lijn")
+with arcpy.da.SearchCursor("temp_buffer_smnooth_lijn", "OID@") as cursor:
+    for row in cursor:
+        list_oid.append(row[0])
+del cursor
+oid = list_oid[-2]
+with arcpy.da.UpdateCursor("temp_buffer_smnooth_lijn", "OID@") as cursor:
+    for row in cursor:
+        if row[0] == oid:
+            pass
+        else:
+            cursor.deleteRow()
+# terugvertaling buffer naar binnenvlak
+arcpy.FeatureToPolygon_management("temp_buffer_smnooth_lijn", "temp_buffer_smnooth_poly")
+
+# clip raster met minibuffer
+arcpy.Clip_management("temp_rastercalc", "", "temp_clip", "temp_buffer_smnooth_poly", "127", "ClippingGeometry", "MAINTAIN_EXTENT")
 # clip to polygon
 arcpy.RasterToPolygon_conversion("temp_clip", "temp_poly", "SIMPLIFY", "Value")
 # remove items with gridcode 0
-with arcpy.da.UpdateCursor("temp_poly", "gridcode") as cursor:
+with arcpy.da.UpdateCursor("temp_poly", ["gridcode","SHAPE@AREA"]) as cursor:
     for row in cursor:
-        if row[0] is 0:
+        if row[0] is 0 or row[1] < 0.1:
             cursor.deleteRow()
         else:
             pass
 
 
 ## middenlijn creeren ##
-# selecteer waterloop lijn in polygoon
-arcpy.MakeFeatureLayer_management(waterlopen, "temp_waterlopen")
-arcpy.SelectLayerByLocation_management("temp_waterlopen", 'intersect', "temp_poly",selection_type="NEW_SELECTION")
-arcpy.CopyFeatures_management("temp_waterlopen","waterlopen_binnenpoly")
-# profielen op lijn van waterloop
-# knip profielen af op rand polygoon
-# punten om de x meter op geknipte profielen
-# haakse profielen op punten
-# profielen knippen op grenzen polyline
+# poly to line
+arcpy.FeatureToLine_management("temp_poly", "temp_poly_lijn")
+# split line at vertices
+arcpy.SplitLine_management("temp_poly_lijn", "temp_poly_lijn_split")
 
-# profielen als input voor centerpoints
+# select only bodempart
+arcpy.MakeFeatureLayer_management("temp_poly_lijn_split", "temp_poly_lijn_split_feat")
+arcpy.SelectLayerByLocation_management("temp_poly_lijn_split_feat", "INTERSECT", lijn, tolerance, "NEW_SELECTION", "INVERT")
+arcpy.CopyFeatures_management("temp_poly_lijn_split_feat", "temp_bodemlijnen_waterloop")
 
-# centerpoints clippen met euclidean distance middenvlak
-
-# centerline bepalen
-
+# z-waarde aan bodemlijn toekennen
