a346d84af9efa038eb55ac889e515504b5eb29f2
Vincent <vincent.vanderwolf@gmail.com>
2020-04-05 22:35:24 +0200

    Aanpassing

--- GIS/waterloopGrid/vergridderWaterloop.py
+++ GIS/waterloopGrid/vergridderWaterloop.py
@@ -12,7 +12,7 @@ distMiniBuffer = -0.2
 tolerance = 0.3
 bodemDiepte = 1
 bodemDiepteSmal = 0.5
-maxBreedteSmal = 2
+maxBreedteSmal = 3
 standaardTalud = 0.5
 
 
@@ -34,6 +34,7 @@ class Basis(object):
             waterloop = "waterloop" + str(idWaterloop)
             bodemLijn = "waterloopBodemlijn" + str(idWaterloop)
             bufferLijn = "waterloopBufferlijn" + str(idWaterloop)
+            waterloopLijn = "waterloopLijn" + str(idWaterloop)
             where = '"' + codeWaterloop + '" = ' + "'" + str(idWaterloop) + "'"
             arcpy.Select_analysis(self.waterlopen, waterloop, where)
             # voeg veld toe voor z_nap
@@ -52,20 +53,19 @@ class Basis(object):
             waterloopPolySmooth = waterloopSmooth[1] # smooth poly
 
             # bepaal insteek (templayer)
-            insteek = gpObject.bepaalInsteek(waterloop,rasterBuitenkant, minLengteSegment, codeWaterloop, waterloopLineSmooth)
-            insteekLijn = insteek[0]
-            insteekHoogte = insteek[1]
+            insteekHoogte = gpObject.bepaalInsteek(waterloop,rasterBuitenkant, minLengteSegment, codeWaterloop, waterloopLineSmooth,waterloopLijn)
+
 
             # bepaal bodemlijn (geen templayer ivm check)
-            gpObject.bepaalBodemlijn(waterloop,insteekLijn, waterloopPolySmooth, distMiniBuffer, tolerance, bodemLijn)
+            gpObject.bepaalBodemlijn(waterloop,waterloopLijn, waterloopPolySmooth, distMiniBuffer, tolerance, bodemLijn)
 
             # bepaal minimale breedte en bodemhoogte
-            bodem = gpObject.bepaalMinimaleBreedte(waterloop, insteekLijn, bodemLijn, insteekHoogte, bodemDiepte, bodemDiepteSmal,maxBreedteSmal)
+            bodem = gpObject.bepaalMinimaleBreedte(waterloop, waterloopLijn, bodemLijn, insteekHoogte, bodemDiepte, bodemDiepteSmal,maxBreedteSmal)
             _bodemDiepte = bodem[0]
             _bodemHoogte = bodem[1]
 
             # buffer waterloop
-            gpObject.bufferWaterloop(waterloop,waterloopPolySmooth,standaardTalud,_bodemDiepte,_bodemHoogte,insteekLijn,bufferLijn)
+            gpObject.bufferWaterloop(waterloop,waterloopPolySmooth,standaardTalud,_bodemDiepte,_bodemHoogte,waterloopLijn,bufferLijn)
 
 
 
