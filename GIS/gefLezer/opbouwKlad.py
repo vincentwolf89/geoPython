228b8a92f1c9008ca13f17104bf5b3036f1c0432
Vincent <vincent.vanderwolf@gmail.com>
2020-04-12 10:24:24 +0200

    Aanpassing

--- GIS/gefLezer/opbouwKlad.py
+++ GIS/gefLezer/opbouwKlad.py
@@ -1,29 +1,39 @@
 import os
 import pandas as pd
 from itertools import cycle
-files = r'C:\Users\Vincent\Desktop\testmap'
+files = r'C:\Users\Vincent\Desktop\testmapBoringen'
 
 soortenGrofGef = ['Z','G']
-maxGrof = 1
+maxGrof = 5
 for file in os.listdir(files):
     naam = file.split('.txt')[0]
     ingef = os.path.join(files, file)
     gef = open(ingef, "r")
 
-
+    # remove blank lines
     lines = (line.rstrip() for line in gef)
-    lines = list(line for line in lines if line)  # Non-blank lines in a list
+    lines = list(line for line in lines if line)
 
-
+    # lijst voor meetgedeelte
     lijstMetingen = []
 
-
+    # ophalen benodigde data
     for item in lines:
+
         if item.startswith('#'):
+            # bepalen separator
             if item.startswith('#COLUMNSEP'):
                 sep = sep = item.strip()[-1:]
-            else:
-                pass
+
+            # ophalen locatiegegevens
+            if item.startswith('#XYID'):
+                coLocs = item.split(',')
+                x = float(coLocs[1])
+                y = float(coLocs[2])
+
+            if item.startswith('#ZID'):
+                zMv = float(item.split(',')[1])
+
         else:
             lijstMetingen.append(item)
 
@@ -100,6 +110,7 @@ for file in os.listdir(files):
     testdct = {'index': indexLijstGrof,'laagNummer': laagNummerLijstGrof, 'bovenkant': bovenkantLijstGrof,'laagdikte':laagdikteLijstGrof}
     testdf = pd.DataFrame(testdct)
 
+
     # print testdf
 
     grouped = testdf.groupby('laagNummer')
@@ -107,24 +118,14 @@ for file in os.listdir(files):
     for group in grouped:
         if group[1]['laagdikte'].max() > maxGrof:
             print group[1]['laagdikte'].max(), group[1]['bovenkant'].min()
+            begrenzing = True
             break
+        else:
+            begrenzing = False
+
+    print begrenzing, file
 
 
-
-    # for i in dct:
-    #
-    #     if dct[i][0] is 0:
-    #         pass
-    #     else:
-    #         laag = dct[i][0]
-    #
-    #         loc = dct[i][1]
-    #         topzand = dct[i][2]
-    #
-    #         if maxGrof <= laag:
-    #             topzand = df.iloc[min(indexLijstGrof)]['bovenkant']
-    #             # print loc, laag, indexLijstGrof
-    #             break
 
 
 
