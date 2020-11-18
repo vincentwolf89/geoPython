aa2c8ac89da61f1297d1c56b6b466ce823ddc91e
Vincent <vincent.vanderwolf@gmail.com>
2020-11-18 13:06:10 +0100

    Plotter...

--- HDSR2020/geometrietoets2020/deel1_schuifsysteem.py
+++ HDSR2020/geometrietoets2020/deel1_schuifsysteem.py
@@ -2,6 +2,7 @@ import arcpy
 from arcpy.sa import *
 from itertools import groupby
 import pandas as pd
+import matplotlib.pyplot as plt 
 
 sys.path.append('HDSR2020')
 from basisRWK2020 import generate_profiles, copy_trajectory_lr, split_profielen
@@ -22,6 +23,9 @@ code_hdsr = "Naam"
 toetsniveaus = "th2024"
 waterlopenBGTBoezem = "bgt_waterdeel_boezem"
 rasterWaterstaatswerk = "WWBAG2mPlusWaterlopenAHN3"
+rasterAHN3BAG2m = r"D:\Projecten\HDSR\2020\gisData\basisData.gdb\BAG2mPlusWaterlopenAHN3"
+
+outputFigures = r"C:\Users\Vincent\Desktop\demoGeomtoets"
 
 def maak_profielen(trajectlijn,code,toetsniveau,profielen,refprofielen, bgt_waterdeel_boezem):
     # referentieprofielen maken
@@ -200,7 +204,7 @@ def bepaal_kruinvlak_toetsniveau(trajectlijn,rasterWaterstaatswerk,toetsniveau,p
 
 
 
-def maak_referentieprofielen(profielen,refprofielen,rasterWaterstaatswerk,toetsniveau, minKruinBreedte):
+def maak_referentieprofielen(profielen,refprofielen,rasterWaterstaatswerk,toetsniveau, minKruinBreedte,refprofielenpunten):
     # maak kruinpunten 
     arcpy.FeatureVerticesToPoints_management("kruindelenTraject", "kruindelenTrajectEindpunten", "BOTH_ENDS")
 
@@ -281,16 +285,120 @@ def maak_referentieprofielen(profielen,refprofielen,rasterWaterstaatswerk,toetsn
     # losse route per profiel maken
 
     arcpy.MakeRouteEventLayer_lr(refprofielen, "profielnummer", "refProfielTabel", "profielnummer POINT afstand", "tempRefProfielen", "", "NO_ERROR_FIELD", "NO_ANGLE_FIELD", "NORMAL", "ANGLE", "LEFT", "POINT")
-    arcpy.CopyFeatures_management("tempRefProfielen", "refProfielenPunten")
-  
+    arcpy.CopyFeatures_management("tempRefProfielen", refprofielenpunten)
+
+
+    # koppelen hoogtewaardes
+    arcpy.AddField_management("refProfielenPunten","z_ref", "DOUBLE", 2, field_is_nullable="NULLABLE")
+
+    refPuntenCursor = arcpy.da.UpdateCursor("refProfielenPunten",["locatie","z_ref"])
+
+    for rRow in refPuntenCursor:
+        if "kruinpunt" in rRow[0]:
+            rRow[1] = toetsniveau
+        if "eindpunt" in rRow[0]:
+            rRow[1] = -15
+        
+        refPuntenCursor.updateRow(rRow)
+
+    del refPuntenCursor
 
 
 
 
 
-    # vanaf hier lokaliseren van refpunten en vervolgens juiste hoogtewaardes koppelen. Dan is het refprofiel klaar.
+
+
+
+def testplot(profielen, refprofielen, refprofielenpunten,rasterAHNBAG):
+
+
+    # stringveld aanmaken voor profielen en refprofielen voor individuele selectie
+    arcpy.AddField_management(profielen,"profielnummer_str","TEXT", field_length=50)
+    arcpy.AddField_management(refprofielen,"profielnummer_str","TEXT", field_length=50)
+    arcpy.AddField_management(refprofielenpunten,"profielnummer_str","TEXT", field_length=50)
+
+    profielCursor = arcpy.da.UpdateCursor(profielen,["profielnummer","profielnummer_str"])
+
+    for pRow in profielCursor:
+        pRow[1] = "profiel_"+str(int(pRow[0]))
+
+        profielCursor.updateRow(pRow)
+    
+    del profielCursor
+
+    profielCursor = arcpy.da.UpdateCursor(refprofielen,["profielnummer","profielnummer_str"])
+
+    for pRow in profielCursor:
+        pRow[1] = "profiel_"+str(int(pRow[0]))
+
+        profielCursor.updateRow(pRow)
+    
+    del profielCursor
+
+    profielCursor = arcpy.da.UpdateCursor(refprofielenpunten,["profielnummer","profielnummer_str"])
+
+    for pRow in profielCursor:
+        pRow[1] = "profiel_"+str(int(pRow[0]))
+
+        profielCursor.updateRow(pRow)
+    
+    del profielCursor
+
     
 
+    # per profielnummer refprofielplotten en gewone profiel plotten
+
+    with arcpy.da.SearchCursor(profielen,["SHAPE@","profielnummer_str"]) as profielCursor:
+        for pRow in profielCursor:
+
+            profielnummer = pRow[1]
+            tempprofiel = "tempprofiel"
+            temprefpunten = "temprefpunten"
+            temprefprofiel = "temprefprofiel"
+            
+            # selecteer betreffend profiel en kopieer naar tijdelijke laag
+            where = '"' + 'profielnummer_str' + '" = ' + "'" + profielnummer + "'"
+            arcpy.Select_analysis(profielen, tempprofiel, where)
+
+            # selecteer referentiepunten van betreffend profiel en kopieer naar tijdelijke laag
+            arcpy.Select_analysis(refprofielenpunten, temprefpunten, where)
+
+            # selecteer betreffend referentieprofiel voor localisatie profielpunten
+            arcpy.Select_analysis(refprofielen, temprefprofiel, where)
+            
+
+
+
+
+
+            # creer routes voor punten profiel
+            arcpy.AddField_management(tempprofiel,"van","DOUBLE", 2, field_is_nullable="NULLABLE")
+            arcpy.AddField_management(tempprofiel,"tot","DOUBLE", 2, field_is_nullable="NULLABLE")
+            
+            tempCursor = arcpy.da.UpdateCursor(tempprofiel, ["van","tot","SHAPE@LENGTH"])
+            for tRow in tempCursor:
+                lengte = tRow[2]
+                tRow[0] = 0
+                tRow[1] = lengte
+                tempCursor.updateRow(tRow)
+            
+            del tempCursor
+            arcpy.CreateRoutes_lr(tempprofiel, "profielnummer_str", 'tempRoute',"TWO_FIELDS", "van", "tot", "UPPER_LEFT", "1", "0", "IGNORE", "INDEX")
+
+            # creer routes voor refprofiel
+            arcpy.AddField_management(temprefprofiel,"van","DOUBLE", 2, field_is_nullable="NULLABLE")
+            arcpy.AddField_management(temprefprofiel,"tot","DOUBLE", 2, field_is_nullable="NULLABLE")
+            
+            tempCursor = arcpy.da.UpdateCursor(temprefprofiel, ["van","tot","SHAPE@LENGTH"])
+            for tRow in tempCursor:
+                lengte = tRow[2]
+                tRow[0] = 0
+                tRow[1] = lengte
+                tempCursor.updateRow(tRow)
+            
+            del tempCursor
+            arcpy.CreateRoutes_lr(temprefprofiel, "profielnummer_str", 'tempRefRoute',"TWO_FIELDS", "van", "tot", "UPPER_LEFT", "1", "0", "IGNORE", "INDEX")
 
 
 
@@ -300,40 +408,82 @@ def maak_referentieprofielen(profielen,refprofielen,rasterWaterstaatswerk,toetsn
 
 
 
-    # nieuwe profielen voor ref: 
-    # arcpy.FeatureVerticesToPoints_management(profielen, "refProfielPunten", "BOTH_ENDS")
+            # profiel voorzien van z-waardes op afstand van 0.5 m
+            arcpy.GeneratePointsAlongLines_management("tempRoute", "puntenRoute", "DISTANCE", Distance= 0.5)
+        
+            # profielpunten lokaliseren
+            arcpy.LocateFeaturesAlongRoutes_lr("puntenRoute", "tempRefRoute", "profielnummer_str", "0,1 Meters", "profileRouteTable", "RID POINT MEAS", "FIRST", "DISTANCE", "ZERO", "FIELDS", "M_DIRECTON")
+            arcpy.JoinField_management("puntenRoute","OBJECTID","profileRouteTable","OBJECTID","MEAS")
+            arcpy.AlterField_management("puntenRoute", 'MEAS', 'afstand')
+
+            # z-waarde aan profielpunten koppelen (indien aanwezig)
+            arcpy.CheckOutExtension("Spatial")
+            ExtractValuesToPoints("puntenRoute", rasterAHNBAG, "puntenRouteZ","INTERPOLATE", "VALUE_ONLY")
+            arcpy.AlterField_management("puntenRouteZ", 'RASTERVALU', 'z_ahn')
+
+
+
+            # plotten 
+            arrayRefProfile = arcpy.da.FeatureClassToNumPyArray("temprefpunten", ('profielnummer','afstand','z_ref'))
+            refDf = pd.DataFrame(arrayRefProfile)
+            sortrefDf = refDf.sort_values(by=['profielnummer','afstand'],ascending=[True, True])
+
+            arrayProfile = arcpy.da.FeatureClassToNumPyArray("puntenRouteZ", ('afstand','z_ahn'))
+            profileDf = pd.DataFrame(arrayProfile)
+            sortProfileDf = profileDf.sort_values(by=['afstand'],ascending=[True])
+
+
+            plt.style.use('seaborn-whitegrid') #seaborn-ticks
+            fig = plt.figure(figsize=(80, 10))
+        
+        
+            ax1 = fig.add_subplot(111, label ="1")
+
+            ax1.plot(sortrefDf['afstand'],sortrefDf['z_ref'],label="Referentie-profiel",linewidth=3)
+            ax1.plot(sortProfileDf['afstand'],sortProfileDf['z_ahn'],label="AHN3-profiel")
+
+
+
+            # plt.show()
+
+            plt.savefig("{}/{}.png".format(outputFigures,profielnummer))
+            plt.close()
+
+            print profielnummer
+
+
+
+
+          
+
+
+
+
+
+
+    # arrayRefProfile = arcpy.da.FeatureClassToNumPyArray("refProfielenPunten", ('profielnummer','afstand','z_ref'))
+    # refDf = pd.DataFrame(arrayRefProfile)
+    # sortrefDf = refDf.sort_values(by=['profielnummer','afstand'],ascending=[True, True])
+
+    # groupedRef = sortrefDf.groupby(["profielnummer",])
+
+    # for name, group in groupedRef:
+        
+    #     plt.style.use('seaborn-whitegrid') #seaborn-ticks
+    #     fig = plt.figure(figsize=(60, 10))
+        
+        
+    #     ax1 = fig.add_subplot(111, label ="1")
+    #     ax1.plot(group['afstand'],group['z_ref'],label="Refprofiel")
+
+
+
+    #     plt.show()
+
 
 
 
     
-    
-    
-    # punten referentieprofielen maken
-    # sr = arcpy.SpatialReference(28992)
-    # arcpy.CreateFeatureclass_management(workspace, "referentieProfielPunten", "POINT",spatial_reference= sr)
-    # arcpy.AddField_management("referentieProfielPunten","profielnummer","DOUBLE", 2, field_is_nullable="NULLABLE")
-    # arcpy.AddField_management("referentieProfielPunten","locatie","TEXT", field_length=50)
-    # arcpy.AddField_management("referentieProfielPunten","z_ref","profielnummer","DOUBLE", 2, field_is_nullable="NULLABLE")
-    # arcpy.AddField_management("referentieProfielPunten","afstand","profielnummer","DOUBLE", 2, field_is_nullable="NULLABLE")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    # begin vanaf buitenkant: 
-
 
 
 
@@ -356,6 +506,8 @@ with arcpy.da.SearchCursor(trajectenHDSR,['SHAPE@',code_hdsr,toetsniveaus]) as c
         refprofielen = "refprofielen"
         trajectlijn = "tempTrajectlijn"
 
+        refprofielenpunten = "refProfielenPunten"
+
         # selecteer betreffend traject en kopieer naar tijdelijke laag
         where = '"' + code_hdsr + '" = ' + "'" + str(id) + "'"
         arcpy.Select_analysis(trajectenHDSR, trajectlijn, where)
@@ -368,4 +520,6 @@ with arcpy.da.SearchCursor(trajectenHDSR,['SHAPE@',code_hdsr,toetsniveaus]) as c
         # bepaal_kruinvlak_toetsniveau(trajectlijn=trajectlijn,rasterWaterstaatswerk=rasterWaterstaatswerk,toetsniveau=toetsniveau,profielen=profielen,refprofielen=refprofielen)
 
         ## stap 3: bepaal referentieprofiel (begin aan buitenzijde)
-        maak_referentieprofielen(profielen=profielen,refprofielen=refprofielen, rasterWaterstaatswerk=rasterWaterstaatswerk,toetsniveau=toetsniveau,minKruinBreedte=minKruinBreedte)
+        # maak_referentieprofielen(profielen=profielen,refprofielen=refprofielen, rasterWaterstaatswerk=rasterWaterstaatswerk,toetsniveau=toetsniveau,minKruinBreedte=minKruinBreedte,refprofielenpunten= refprofielenpunten)
+
+        testplot(profielen=profielen,refprofielen=refprofielen, refprofielenpunten=refprofielenpunten,rasterAHNBAG=rasterAHN3BAG2m)
